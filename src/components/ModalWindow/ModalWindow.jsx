6ba240439734c429a06706bb83c5d15ee1fde700
Kateryna <k.churuta@gmail.com>
2024-05-07 17:54:24 +0100

    start working on ModalWindow

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -1,8 +1,10 @@
 
 
 export const ModalWindow = ({ selectedItem, onClose }) => {
+    console.log(selectedItem)
     return (
         <>
+            <button onClick={onClose}></button>
             modal here
 
         </>
