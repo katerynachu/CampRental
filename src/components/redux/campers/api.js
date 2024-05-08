ecc2208fe7b8f47c0e813b92a2baf625e48918f3
Kateryna <k.churuta@gmail.com>
2024-05-08 19:48:29 +0100

    Update api.js

--- src/components/redux/campers/api.js
+++ src/components/redux/campers/api.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const fetchCampers = createAsyncThunk(
-    'campers/fetchCampers', // Новий назва дії
+    'campers/fetchCampers',
     async () => {
         try {
             const response = await axios.get('https://65fc90ee9fc4425c65306aba.mockapi.io/campers/campers');
             const camperData = response.data;
             console.log(camperData);
-            return camperData; // Повертаємо дані, щоб вони потрапили у поле payload дії
+            return camperData;
         } catch (error) {
             console.error('Error fetching camper data:', error);
-            throw error; // Передаємо помилку назад, щоб вона оброблялась на рівні компоненту
+            throw error;
         }
     }
 );
